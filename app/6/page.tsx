f286f8283937f9409f4d27900ba6ac3632803dca
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2024-05-02 01:50:51 +0600

    button animation added 2

--- app/6/page.tsx
+++ app/6/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import FAQ from '@/components/pageThreeComponents/FAQ'
+import Discover from '@/components/pageThreeComponents/Discover'
+import Explore from '@/components/pageThreeComponents/Explore'
+import Points from '@/components/pageThreeComponents/Points'
+import NFTs from '@/components/pageThreeComponents/NFTs'
+import NFTShowcase from '@/components/pageThreeComponents/NFTShowcase'
+import Review from '@/components/pageThreeComponents/Review'
+import CTA from '@/components/pageThreeComponents/CTA'
+import Contact from '@/components/pageThreeComponents/Contact'
 
 function page() {
     return (
         <div className='bg-gray-900 text-white'>
-            <h1>Page</h1>
+            <Discover />
+            <Explore />
+            <Points />
+            <NFTs />
+            <NFTShowcase />
+            <Review />
+            <FAQ />
+            <CTA />
+            <Contact />
         </div>
     )
 }
